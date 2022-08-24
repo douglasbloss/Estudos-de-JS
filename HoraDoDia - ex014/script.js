4a9662dc54b91afb3c5b83c35d16392dac11dd13
Douglas <douglasbloss@gmail.com>
2022-08-24 18:24:01 -0300

    Novos exercicios

--- HoraDoDia - ex014/script.js
+++ HoraDoDia - ex014/script.js
@@ -3,6 +3,7 @@ function carregar() {
   var img = window.document.getElementById("foto");
   var data = new Date();
   var hora = data.getHours()
+  // var hora = 14;
   // var hora = 19;
   msg.innerHTML = `Agora são ${hora} horas.`;
   if (hora >= 0 && hora < 12) {
