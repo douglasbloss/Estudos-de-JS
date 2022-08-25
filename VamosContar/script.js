74a2f2cd32f206e951acd5026c7d5ba84fddc1cd
Douglas <douglasbloss@gmail.com>
2022-08-25 10:05:07 -0300

    Update script.js

--- VamosContar/script.js
+++ VamosContar/script.js
@@ -11,19 +11,24 @@ function contar() {
     passo.value.length == 0
   ) {
     window.alert("[ERRO] Faltam dados!");
-    res.innerHTML = 'Impossível contar!'
+    res.innerHTML = "Impossível contar!";
   } else {
-    res.innerHTML = `Contando: `;
+    res.innerHTML = `Contando: <br>`;
     let i = Number(ini.value);
     let f = Number(fim.value);
     let p = Number(passo.value);
+    if (p <= 0) {
+      window.alert(
+        "Passo não pode ser igual ou menor do que zero. \nConsiderando PASSO 1"
+      );
+      p = 1;
+    }
     if (i < f) {
       for (let c = i; c <= f; c += p) {
         res.innerHTML += ` ${c} \u{1F449}`;
       }
     } else {
-      for (let c = i; c >= f; c -= p) 
-      res.innerHTML += ` ${c} \u{1F449}`;
+      for (let c = i; c >= f; c -= p) res.innerHTML += ` ${c} \u{1F449}`;
     }
     res.innerHTML += ` \u{1F3C1}`;
   }
